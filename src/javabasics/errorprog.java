1012824255ea0561aeee76f1a4b483d9e9987b5f
Sugan181045 <sugan181045@gmail.com>
2018-05-30 00:20:01 -0700

    error1

---
+++ src/javabasics/errorprog.java
@@ -0,0 +1,17 @@
+package javabasics;
+
+public class errorprog {
+	public static void main(String a[]){
+		
+	int age=70;
+ if ( age >= 65 )
+ {
+System.out.println( "Age is greater than or equal to 65" );
+ }
+else
+{
+System.out.println( "Age is less than 65 ");
+}
+
+}
+}
