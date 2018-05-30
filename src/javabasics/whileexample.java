51fc10b56cf693d2fa8a94d6d44373165d7ffc8b
Sugan181045 <sugan181045@gmail.com>
2018-05-29 23:58:04 -0700

    whilesample

---
+++ src/javabasics/whileexample.java
@@ -0,0 +1,14 @@
+package javabasics;
+
+public class whileexample {
+	public static void main(String a[]) {
+		int num=10;
+		while(num>2)
+		{
+			System.out.println(num);
+			num--;
+		}
+	}
+
+}
+
