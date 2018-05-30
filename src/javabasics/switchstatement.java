7bae8377be7b95e11dee48d7f1e6ff02ea46853e
Sugan181045 <sugan181045@gmail.com>
2018-05-29 23:48:53 -0700

    switchsample

---
+++ src/javabasics/switchstatement.java
@@ -0,0 +1,21 @@
+package javabasics;
+
+public class switchstatement {
+	public static void main(String a[]) {
+		int num;
+		num=2;
+		switch(num) {
+		case 1:
+			System.out.println("good");
+		break;
+		case 2:
+			System.out.println("bad");
+			break;
+			
+		default:
+			System.out.println("default");
+			
+		}
+	}
+
+}
