84dd266a82accb9912487edab39e4de042dc0b7f
Sugan181045 <sugan181045@gmail.com>
2018-05-30 01:49:05 -0700

    error2

---
+++ src/javabasics/errorprog2.java
@@ -0,0 +1,15 @@
+package javabasics;
+
+public class errorprog2 {
+	public static void main(String a[]) {
+		
+	int x = 1, total=0;
+	while ( x <= 10 )
+	{
+	total += x;
+	++x;
+	
+System.out.println("total= "+total);
+}
+	}
+}
