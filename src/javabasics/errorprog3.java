a49ea5b5cafaacad16ab5c99a7f98606424339c9
Sugan181045 <sugan181045@gmail.com>
2018-05-30 01:58:40 -0700

    error3

---
+++ src/javabasics/errorprog3.java
@@ -0,0 +1,15 @@
+package javabasics;
+
+public class errorprog3 {
+public static void main(String a[]) {
+	int x=0;
+	int total=0;
+
+while ( x <= 100 )
+{
+total += x;
+++x;
+System.out.println("Total= "+total);
+}
+}
+}
