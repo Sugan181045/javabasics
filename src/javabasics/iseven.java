d0512527aa8a8b2575c53a37ccacff1fdd5e23c2
Sugan181045 <sugan181045@gmail.com>
2018-05-31 05:56:00 -0700

    iseven

---
+++ src/javabasics/iseven.java
@@ -0,0 +1,11 @@
+package javabasics;
+
+public class iseven {
+	public boolean iseven1(int one) {
+		return one%2==0?true:false;
+	}
+public static void main(String a[]) {
+	iseven a1=new iseven();
+	System.out.println(a1.iseven1(7));
+}
+}
