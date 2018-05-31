c89b5d43ddd5eea8df254c5e3bbb56e1a7703c75
Sugan181045 <sugan181045@gmail.com>
2018-05-31 02:08:26 -0700

    classcar

---
+++ src/javabasics/classcar.java
@@ -0,0 +1,18 @@
+package javabasics;
+
+public classcar {
+	String model;
+	double speed;
+	
+	public void accelerate() {
+		speed=10;
+	}
+public static void main(String a[]) {
+	car car1=new car();
+	car car2=new car();
+   car1.speed=20.0;
+   car2.speed=30.0;
+	System.out.println(car1.speed);
+	System.out.println(car2.speed);;
+}
+}
