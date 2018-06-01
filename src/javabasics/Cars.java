888edfc762c551f044384a53412d34c1196c65a3
Sugan181045 <sugan181045@gmail.com>
2018-06-01 05:52:51 -0700

    carclass1

--- src/javabasics/Cars.java
+++ src/javabasics/Cars.java
@@ -7,7 +7,13 @@ public class Cars {
 	public Cars(int i) {
 		System.out.println("one arg cons called");
 	}
+	public void brake()
+	{
+		System.out.println("breake is used");
+	}
 public static void main(String a[]) {
 	Cars a1= new Cars(10);
+	a1.brake();
 }
+
 }
