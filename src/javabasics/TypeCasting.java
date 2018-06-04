d643c994aa1d1fa8503ac711494f7fab0c830b0e
Sugan181045 <sugan181045@gmail.com>
2018-06-04 02:01:31 -0700

    typecasting

---
+++ src/javabasics/TypeCasting.java
@@ -0,0 +1,12 @@
+package javabasics;
+
+public class TypeCasting {
+	public static void main(String a[]) {
+	
+	byte one=5;
+	double d=12.1;
+	double done=d+one;
+	
+		System.out.println(done);
+	}
+}
