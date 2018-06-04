fa91e8774922b41ac3da016c6a5ad3a830255468
Sugan181045 <sugan181045@gmail.com>
2018-06-04 05:13:58 -0700

    taxi

--- src/javabasics/Taxi.java
+++ src/javabasics/Taxi.java
@@ -4,14 +4,17 @@ public class Taxi  extends Cars{
 	public Taxi() {
 		System.out.println("Taxi cons empty");
 	}
-    public Taxi(int i) {
+    public Taxi(int speed) {
+    	
+    	
     	System.out.println("Taxi cons one arg");
     }
-    public void brake() {
+    public void brake() { 
+        
     	System.out.println("brake in taxi called");
-    }
+   }
     public static void main(String a[]) {
-    	Taxi t1=new Taxi(10);
+    	Taxi t1=new Taxi();
     	t1.brake();
     	
     }
--- src/javabasics/typecast2.java
+++ src/javabasics/typecast2.java
@@ -2,7 +2,8 @@ package javabasics;
 
 public class typecast2 {
 	public static void main(String a[]) {
-		byte b1=3;short s1=130;
+		byte b1=3;
+		short s1=130;
 		s1=b1;
 		b1=(byte)s1;
 		System.out.println(s1);
