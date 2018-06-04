8ccc8f76d4c6015c80ff3e57a9e3606ed916a433
Sugan181045 <sugan181045@gmail.com>
2018-06-04 05:11:37 -0700

    passbyvalue

---
+++ src/javabasics/PassByValue.java
@@ -0,0 +1,22 @@
+package javabasics;
+
+public class PassByValue {
+	
+	public static void main(String a[]) {
+		one obj= new one();
+		int d=obj.c(5,2);
+		System.out.println(d);
+	}
+	
+}
+	
+	
+	
+	class one{
+		public  int c(int a,int b) {
+			int c=a*b;
+			return c;
+		
+	}
+
+}
