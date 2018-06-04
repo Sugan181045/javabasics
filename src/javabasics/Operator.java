b55caaccad1daed1f3c15bed3876f0fbee642452
Sugan181045 <sugan181045@gmail.com>
2018-06-04 02:39:50 -0700

    operator

---
+++ src/javabasics/Operator.java
@@ -0,0 +1,17 @@
+package javabasics;
+
+public class Operator {
+public static void main(String a[]) {
+	int num=12;
+	int total=9;
+	num++;
+	--num;
+	total+=num;
+	int negativeCount=-7;
+	System.out.println(num!=11);
+	System.out.println(total);
+	System.out.println(~negativeCount);
+	System.out.println(num & total );
+	System.out.println( num^total);
+}
+}
