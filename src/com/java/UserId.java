d999bfc87f00aa9194da928e0311007d9cdc5b52
Sugan181045 <sugan181045@gmail.com>
2018-07-08 16:33:25 +0530

    userid

---
+++ src/com/java/UserId.java
@@ -0,0 +1,23 @@
+package com.java;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class UserId {
+	public static void main(String[] args) {
+		//char[] smaller) {
+		/*String s1="Rajiv";
+		String s2="Roy";
+		System.out.println(s1.length());
+		System.out.println(s2.length());
+		String s1.length() =new String(s3);
+		}
+	}*/
+	   // class Simple{  
+	        //public static void main(String[] args){  
+	        //Overflow  
+	        int a=133;  
+	        byte b=(byte)a;  
+	        System.out.println(a);  
+	        System.out.println(b);  
+	        }}  
